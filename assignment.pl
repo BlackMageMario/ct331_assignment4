71b38f3b649ee778985a023fdb1c927695e9a102
EAMONN FRANCIS HANNON <15310091@it.nuigalway.ie>
2017-11-16 14:27:17 +0000

    Final commit: code working with adjusted assignment parameters.

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,26 @@
 %Definitions for isElementInList(El, List)
-
-%Definitions for reverseList(List, ReversedList)
-
-%insertElementIntoListEnd(El, List, NewList)
-
-%Definitions for mergeLists(List1, List2, Merged)
+% we can actually call our base case the same as the function name.
+isElementInList(X, [X|_]).
+isElementInList(El, List) :-
+	[_|T] = List,
+	isElementInList(El, T).
+% base case: empty list combined with another list = merged list
+mergeLists([], X, X).	
+% get tails of both lists, and merge them together
+mergeLists(List1, List2, Merged) :-
+	[H|T1] = List1, 
+	[H|T2] = Merged,
+	mergeLists(T1, List2, T2).
+% base case: both empty lists
+% essentially: keep reversing the list until we have empty lists
+% then append the head of the last list to the reversed list, and
+% go back up the chain, until we return the reversed list
+reverseList([], []).
+reverseList(List, ReversedList):-
+	[H|T] = List,
+	reverseList(T, RevT), 
+	mergeLists(RevT, [H], ReversedList).	
+% Very simple - just merge the element onto the end of the second list
+insertElementIntoListEnd(El, List, NewList) :-
+	mergeLists(List, [El], NewList).
+	
